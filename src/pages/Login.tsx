8b1016d5435780f98d59adb6e38ae45e1648051c
Kennedy Ramos <devnedy@gmail.com>
2025-11-15 13:54:43 -0300

    Feat material-ui-insert

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,30 @@
-import styled from "styled-components";
+import { Box, Container, Grid } from '@mui/material'
+import { BannerImage } from '@/components'
 
-const LoginArea = styled.div`
-    background: #666;
-`
-
-const LoginImage = styled.div`
-    background-image: url(/login-image.svg);
-    background-size: cover;
-    height: 100vh;
-    width: 50vw;
-`
 
 export default function Login() {
-
   return (
     <>
-        <LoginArea>LOGIN</LoginArea>
-        <LoginImage />
+      <Box>
+        <Grid container>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            sx={{ display: 'flex', alignItems: 'center', height: '100vh' }}
+          >
+            <Container maxWidth="sm">
+              <h1>Login</h1>
+            </Container>
+          </Grid>
+
+          <Grid item sm={6} sx={{ display: { xs: 'none', sm: 'block' } }}>
+            <Container>
+              <BannerImage />
+            </Container>
+          </Grid>
+        </Grid>
+      </Box>
     </>
   )
 }
