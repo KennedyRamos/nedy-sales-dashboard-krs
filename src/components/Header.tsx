d172b9b670af0dfc07ba32c217d04ae61c503532
Kennedy Ramos <devnedy@gmail.com>
2025-11-17 14:25:21 -0300

    feat: Header Component

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import { Avatar, Box, Container } from '@mui/material'
+import { Link } from 'react-router-dom'
+import { Logo } from '@/components'
+import styled from 'styled-components'
+import { pxToRem } from '@/utils'
+
+
+const StyledHeader = styled.header`
+  background-color: ${(props => props.theme.appBackground)};
+  border-bottom: ${pxToRem(1)} solid ${(props) => props.theme.appDefaltStroke};
+  margin-bottom: ${pxToRem(37)};
+  background-size: cover;
+  width: 100%;
+`
+
+export default function Header() {
+    return (
+        <StyledHeader>
+            <Container maxWidth='lg'>
+                <Box sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    height: pxToRem(64),
+                }}>
+                    <Link to='/home'>
+                        <Logo height={35} whidth={35}></Logo>
+                    </Link>
+                    
+                    <Link to='/perfil'>
+                        <Avatar alt='pefil' src='/perfil.svg' sx={{ width: pxToRem(45), height: pxToRem(45)}} />
+                    </Link>
+                </Box>
+            </Container>
+        </StyledHeader>
+    )
+}
